ec4b33ef17b152ad88d695ac137a083bf4fa4d48
litao <c-litao1@genomics.cn>
2020-07-20 18:20:05 +0800

    editor

--- src/Components/Editor/messageBoard.js
+++ src/Components/Editor/messageBoard.js
@@ -2,13 +2,14 @@
  * @Author: Litao 
  * @Date: 2020-07-07 09:41:28 
  * @Last Modified by: Litao
- * @Last Modified time: 2020-07-07 16:11:55
+ * @Last Modified time: 2020-07-20 18:19:46
  */
 import React from 'react'
 import { Input } from 'antd'
 import { SmileOutlined } from '@ant-design/icons'
 
 import MyDialog from './EditorDialog/myDialog'
+import './index.scss'
 
 const { TextArea } = Input
 
@@ -16,26 +17,109 @@ class MessageBoard extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            showDialog: false
+            showDialog: false,
+            iconPic: ['icon-biaoqing', 'icon-jiacu', 'icon-xieti', 'icon-zitiyanse', 'icon-beijingse', 'icon-tupian', 'icon-chaolianjie', 'icon-shouhangsuojin', 'icon-zuoduiqi', 'icon-ziyuan1', 'icon-youduiqi'],
         }
     }
 
     getExpression = () => {
         this.setState({
-            showDialog: true
+            // showDialog: true
         })
     }
+
+    textareaSelect = (e) => {
+        console.log(e)
+    }
+
+    getEditorType = (e) => {
+        switch (e) {
+            case 'icon-biaoqing':
+                this.biaoqing()
+                break;
+            case 'icon-jiacu':
+                this.jiacu()
+                break;
+            case 'icon-xieti':
+                this.xieti()
+                break;
+            case 'icon-zitiyanse':
+                this.zitiyanse()
+                break;
+            case 'icon-beijingse':
+                this.beijingse()
+                break;
+            case 'icon-tupian':
+                this.tupian()
+                break;
+            case 'icon-chaolianjie':
+                this.chaolianjie()
+                break;
+            case 'icon-shouhangsuojin':
+                this.shouhangsuojin()
+                break;
+            case 'icon-zuoduiqi':
+                this.zuoduiqi()
+                break;
+            case 'icon-ziyuan1':
+                this.ziyuan1()
+                break;
+            case 'icon-youduiqi':
+                this.youduiqi()
+                break;
+            default:
+                break;
+        }
+    }
+    biaoqing = () => {
+
+    }
+    jiacu = () => {
+        console.log("加粗")
+
+    }
+    xieti = () => {
+        
+    }
+    zitiyanse = () => {
+        
+    }
+    beijingse = () => {
+        
+    }
+    tupian = () => {
+        
+    }
+    chaolianjie = () => {
+        
+    }
+    shouhangsuojin = () => {
+        
+    }
+    zuoduiqi = () => {
+        
+    }
+    ziyuan1 = () => {
+        
+    }
+    youduiqi = () => {
+        
+    }
     render() {
-        const { showDialog } = this.state
+        const { showDialog, iconPic } = this.state
+        const Icon = iconPic.map((item, index) => {
+            return <i className={"iconfont " + item + " iconfontStyle"} key={index} onClick={() => { this.getEditorType(item) }} ></i>
+        })
         return (
             <div className="myEditor">
                 <div className="myEditorTop">
-                    <SmileOutlined style={{ fontSize: '1.6em', cursor: 'pointer' }} onClick={this.getExpression} />
+                    {
+                        Icon
+                    }
                 </div>
                 <div className="myEditorBottom">
-                    <TextArea rows={4} ></TextArea>
+                    <textarea className="iconTextareaStyle" rows={10} onSelect={(e) => {this.textareaSelect(e)}}></textarea>
                 </div>
-                <MyDialog showDialog={showDialog}/>
             </div>
         )
     }
